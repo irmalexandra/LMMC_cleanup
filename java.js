23734123da7fbad63faf82eb335ae5e779f45b9a
lokialex <lokih@ru.is>
2020-08-24 12:01:37 +0000

    updates to project

--- java.js
+++ java.js
@@ -1,5 +1,6 @@
 const proxy_url = 'http://localhost:5000/api/v1/'
 
+
 //https://github.com/Tautulli/Tautulli/blob/master/API.md
 
 let libraryList = document.getElementById('library-list');
@@ -8,38 +9,80 @@ loadLibraryList();
 
 function loadLibraryList() {
   const req = new XMLHttpRequest();
-  req.open('GET', proxy_url + "get_library_names", true )
-  if (this.status === 200) {
-      req.onload = function() {
+  req.open('GET', proxy_url + "get_library_names", true );
+  req.onload = function() {
+    if (this.status === 200) {
         const data = JSON.parse(this.responseText);
-        libraryJSON = data.response.data
-        console.log(libraryJSON)
-        for (i = 0; i < libraryJSON.length; i++) {
-            section_id = libraryJSON[i].section_id;
-            section_name = libraryJSON[i].section_name;
+        let libraryJSON = data.response.data;
+        for (let i = 0; i < libraryJSON.length; i++) {
+            let section_id = libraryJSON[i].section_id;
+            let section_name = libraryJSON[i].section_name;
 
             let radioOption = document.createElement('input');
             radioOption.type = 'radio';
-            radioOption.id = 'library_' + section_id;
+            radioOption.id = section_id;
+            radioOption.name = 'library';
+            radioOption.className = 'library';
 
-            let radioLabel = document.createElement('label')
+            let radioLabel = document.createElement('label');
 
-            radioLabel.htmlFor = 'default'
-            radioLabel.innerHTML = section_name
-            libraryList.appendChild(radioOption)
+            radioLabel.htmlFor = 'default';
+            radioLabel.innerHTML = section_name;
+            libraryList.appendChild(radioOption);
             libraryList.appendChild(radioLabel)
         }
-      }
-  }
-  else {
-      document.getElementById('library-list-text').innerHTML ='Error! Tautulli data could not be accessed.';
-  };
-
+    }
+    else {
+        document.getElementById('library-list-text').innerHTML ='Error! Tautulli data could not be accessed.';
+    }
+    };
   req.send();
 }
 
-function handleDateCutoff() {
+function getMediaList(libID, cutoffDate, res){
+    const req = new XMLHttpRequest();
+    req.open('POST', proxy_url + 'get_library_media_info/' + libID, true);
+    console.log("inb4 onload")
+    req.onload = function () {
+        console.log("where we at")
+        if (this.status === 200) {
+            const data = JSON.parse(this.responseText);
+            let mediaJSON = data.response.data;
+            console.log(mediaJSON)
+        }
+        else {
+            console.log('something went wrong')
+        }
+    };
+}
 
-};
 
-//attr('value', entry.section_id).text(entry.section_name));
+let submit = document.getElementById("form-submit")
+submit.addEventListener("click", function(event){
+    event.preventDefault()
+
+    let libArr = document.getElementsByClassName('library')
+    let chosenLibID = ''
+    for (let i = 0; i < libArr.length; i++) {
+        if (libArr[i].checked === true) {
+            chosenLibID = libArr[i].id
+            i = libArr.length
+        }
+    };
+
+    let resArr = document.getElementsByClassName("resolution")
+    let chosenQuality = ""
+    for (let i = 0; i < resArr.length; i++) {
+        if (resArr[i].checked === true) {
+            chosenQuality = resArr[i].id
+            i = resArr.length
+        }
+    };
+
+    let cutoffDate = document.getElementById("cutoff-date").value
+
+    getMediaList(parseInt(chosenLibID), cutoffDate, chosenQuality)
+});
+
+
+
--- proxy_folder/cors_proxy.js
+++ proxy_folder/cors_proxy.js
@@ -3,10 +3,10 @@ const request = require('request');
 
 const app = express();
 
-const ip = "130.208.183.18"
-const port = ":8181"
-const api_key = "c32952c9371049eb8d54cc72978c4533"
-const api_start = "/api/v2?apikey="
+const ip = "130.208.183.18";
+const server_port = ":8181";
+const api_key = "c32952c9371049eb8d54cc72978c4533";
+const api_start = "/api/v2?apikey=";
 
 
 app.use((req, res, next) => {
@@ -16,25 +16,26 @@ app.use((req, res, next) => {
 
 app.get('/api/v1/get_library_names', (req, res) => {
   request(
-    { url: 'http://' + ip + port + api_start + api_key + "&cmd=get_library_names" },
+    { url: 'http://' + ip + server_port + api_start + api_key + "&cmd=get_library_names" },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({ type: 'error', message: err.message });
       }
-      console.log("somethings happening")
+      console.log("get library list successful");
       res.json(JSON.parse(body));
     }
   )
 });
 
-app.get('/api/v1/get_library_media_info/:library_id/:section_type', (req, res) => {
+app.get('/api/v1/get_library_media_info/:libraryID', (req, res) => {
+  let libraryID = req.params.libraryID
   request(
-    { url: 'http://' + ip + port + api_start + api_key + "&cmd=get_library_names" },
+    { url: 'http://' + ip + server_port + api_start + api_key + "&cmd=get_library_names", req: libraryID },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({ type: 'error', message: err.message });
       }
-      console.log("somethings happening")
+      console.log("get library contents successful")
       res.json(JSON.parse(body));
     }
   )
